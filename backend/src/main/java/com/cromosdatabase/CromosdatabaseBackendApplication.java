1a437c224e77e818d667974f20d80156fca68af8
mherrero <mighelo1985@gmail.com>
2026-03-22 01:05:38 +0100

    Creación de backend - Setup inicial Spring Boot con algunas dependencias.

---
+++ backend/src/main/java/com/cromosdatabase/CromosdatabaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cromosdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CromosdatabaseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CromosdatabaseBackendApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/cromosdatabase/CromosdatabaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cromosdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CromosdatabaseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
