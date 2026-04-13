3ea441725e9dc2c694a91508aed65aec78906d0b
mherrero <mighelo1985@gmail.com>
2026-04-13 22:41:04 +0200

    Editoriales: Adaptación de repositorio y creación de filtros.

--- backend/src/main/java/com/cromosdatabase/repositorios/EditorialRepository.java
+++ backend/src/main/java/com/cromosdatabase/repositorios/EditorialRepository.java
@@ -2,13 +2,15 @@ package com.cromosdatabase.repositorios;
 
 import com.cromosdatabase.modelo.entidades.Editorial;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
 /**
  * Repositorio JPA para la entidad Editorial.
  */
-public interface EditorialRepository extends JpaRepository<Editorial, Integer> {
+public interface EditorialRepository extends JpaRepository<Editorial, Integer>,
+        JpaSpecificationExecutor<Editorial> {
 
     /**
      * Busca una editorial por su nombre exacto.
