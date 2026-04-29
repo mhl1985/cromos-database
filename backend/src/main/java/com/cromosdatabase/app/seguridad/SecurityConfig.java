eb18d326d6ce9dbfc8b98d83ff5cce01b9285c88
mherrero <mighelo1985@gmail.com>
2026-04-29 19:30:37 +0200

    Añadido de orígenes permitidos en SecurityConfig

--- backend/src/main/java/com/cromosdatabase/app/seguridad/SecurityConfig.java
+++ backend/src/main/java/com/cromosdatabase/app/seguridad/SecurityConfig.java
@@ -141,6 +141,8 @@ public class SecurityConfig {
         // Orígenes permitidos para acceder al backend
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:5500",
+                "http://localhost",
+                "http://localhost:80",
                 "http://127.0.0.1:5500",
                 "http://localhost:3000",
                 "http://localhost:5173"
