4a0199a05a42f9cefdf1d42a2d0bc68ae6b0ca35
mherrero <mighelo1985@gmail.com>
2026-05-02 02:13:32 +0200

    Endpoint página Colección: Añadido de endpoint en controller para páginas específicas.

--- backend/src/main/java/com/cromosdatabase/controladores/PaginaController.java
+++ backend/src/main/java/com/cromosdatabase/controladores/PaginaController.java
@@ -1,11 +1,14 @@
 package com.cromosdatabase.controladores;
 
+import com.cromosdatabase.modelo.dtos.paginas.ColeccionPageResponse;
 import com.cromosdatabase.modelo.dtos.paginas.PaginaInicioResponse;
+import com.cromosdatabase.servicios.PaginaColeccionService;
 import com.cromosdatabase.servicios.PaginaInicioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +30,11 @@ public class PaginaController {
     private final PaginaInicioService paginaInicioService;
 
     /**
+     * Servicio de carga de la página de colección.
+     */
+    private final PaginaColeccionService paginaColeccionService;
+
+    /**
      * Carga la página de inicio.
      *
      * Endpoint público:
@@ -39,6 +47,27 @@ public class PaginaController {
     public ResponseEntity<PaginaInicioResponse> cargarPaginaInicio() {
 
         PaginaInicioResponse response = paginaInicioService.cargarPaginaInicio();
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    /**
+     * Carga la página de una colección.
+     *
+     * Endpoint público:
+     * - Sin token: devuelve información pública de la colección.
+     * - Con token válido: Se añade información de los cromos que el usuario
+     * tiene de esa colección.
+     *
+     * @param idColeccion id de la colección
+     * @return datos necesarios para pintar la página de colección
+     */
+    @GetMapping("/colecciones/{idColeccion}")
+    public ResponseEntity<ColeccionPageResponse> cargarPaginaColeccion(
+            @PathVariable Integer idColeccion) {
+
+        ColeccionPageResponse response =
+                paginaColeccionService.cargarPaginaColeccion(idColeccion);
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
