209e9e67c8b0d1c1e39f62a37e02152a22e7fbfa
mherrero <mighelo1985@gmail.com>
2026-05-21 00:56:51 +0200

    Inclusión de imgs genéricas para por categorías.

--- docs/BD/LOCAL_002_update_bd_cromos_database.sql
+++ docs/BD/LOCAL_002_update_bd_cromos_database.sql
@@ -2,12 +2,36 @@
 -- Script: 002_update_bd_cromos_database.sql
 --   Ejecutar después del insert inicial.
 -- =========================================================
---
--- =========================================================
--- IMGs PORTADAS COLECCIONES DE FÚTBOL
--- =========================================================
+
 USE cromos_database;
 
+-- =========================================================
+-- IMGs GENÉRICAS POR CATEGORÍA
+-- =========================================================
+
+UPDATE colecciones
+SET url_img_portada = 'img/muestras/portada_categoria_deportes.webp'
+WHERE id_categoria = 1;
+
+UPDATE colecciones
+SET url_img_portada = 'img/muestras/portada_categoria_anime.webp'
+WHERE id_categoria = 2;
+
+UPDATE colecciones
+SET url_img_portada = 'img/muestras/portada_categoria_cine_television.webp'
+WHERE id_categoria = 3;
+
+UPDATE colecciones
+SET url_img_portada = 'img/muestras/portada_categoria_videojuegos.webp'
+WHERE id_categoria = 4;
+
+UPDATE colecciones
+SET url_img_portada = 'img/muestras/portada_categoria_historia_cultura.webp'
+WHERE id_categoria = 5;
+
+-- =========================================================
+-- IMGs PORTADAS REALES - COLECCIONES DE FÚTBOL
+-- =========================================================
 UPDATE colecciones
 SET url_img_portada = 'img/colecciones/1_panini-fifa-world-cup-qatar-2022_portada.webp'
 WHERE id_coleccion = 1;
