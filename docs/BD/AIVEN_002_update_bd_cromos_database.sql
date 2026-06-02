99ef55a249959ae7fae4eca83fcc5de18eed6723
mherrero <mighelo1985@gmail.com>
2026-06-02 09:55:09 +0200

    Modificación archivos SQL de actualización para añadir urls de img de cromos de 2 colecciones.

--- docs/BD/AIVEN_002_update_bd_cromos_database.sql
+++ docs/BD/AIVEN_002_update_bd_cromos_database.sql
@@ -294,6 +294,110 @@ SET url_img_delantera = 'img/cromos/2_panini-fifa-world-cup-russia-2018/187_288_
 WHERE id_cromo = 187;
 
 -- =========================================================
+-- IMGs CROMOS REALES - (ID 3) - BRAZIL 2014
+-- =========================================================
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/201_00_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/201_00_trasera.webp'
+WHERE id_cromo = 201;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/202_1_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/202_1_trasera.webp'
+WHERE id_cromo = 202;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/203_2_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/203_2_trasera.webp'
+WHERE id_cromo = 203;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/204_3_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/204_3_trasera.webp'
+WHERE id_cromo = 204;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/205_4_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/205_4_trasera.webp'
+WHERE id_cromo = 205;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/206_5_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/206_5_trasera.webp'
+WHERE id_cromo = 206;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/207_6_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/207_6_trasera.webp'
+WHERE id_cromo = 207;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/208_7_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/208_7_trasera.webp'
+WHERE id_cromo = 208;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/209_8_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/209_8_trasera.webp'
+WHERE id_cromo = 209;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/210_9_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/210_9_trasera.webp'
+WHERE id_cromo = 210;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/211_10_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/211_10_trasera.webp'
+WHERE id_cromo = 211;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/212_11_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/212_11_trasera.webp'
+WHERE id_cromo = 212;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/213_12_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/213_12_trasera.webp'
+WHERE id_cromo = 213;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/214_13_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/214_13_trasera.webp'
+WHERE id_cromo = 214;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/215_14_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/215_14_trasera.webp'
+WHERE id_cromo = 215;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/216_15_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/216_15_trasera.webp'
+WHERE id_cromo = 216;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/217_16_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/217_16_trasera.webp'
+WHERE id_cromo = 217;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/218_17_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/218_17_trasera.webp'
+WHERE id_cromo = 218;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/219_18_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/219_18_trasera.webp'
+WHERE id_cromo = 219;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/220_19_delantera.webp',
+    url_img_trasera = 'img/cromos/3_panini-fifa-world-cup-brazil-2014/220_19_trasera.webp'
+WHERE id_cromo = 220;
+
+-- =========================================================
 -- IMGs CROMOS REALES - (ID 4) - FRANCE 98
 -- =========================================================
 
@@ -396,3 +500,112 @@ UPDATE cromos
 SET url_img_delantera = 'img/cromos/4_panini-fifa-world-cup-france-98/320_20_delantera.webp',
     url_img_trasera = 'img/cromos/4_panini-fifa-world-cup-france-98/320_20_trasera.webp'
 WHERE id_cromo = 320;
+
+-- =========================================================
+-- IMGs CROMOS REALES - (ID 5) - USA 94
+-- =========================================================
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/401_1_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/401_1_trasera.webp'
+WHERE id_cromo = 401;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/402_2_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/402_2_trasera.webp'
+WHERE id_cromo = 402;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/403_3_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/403_3_trasera.webp'
+WHERE id_cromo = 403;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/404_4_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/404_4_trasera.webp'
+WHERE id_cromo = 404;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/405_5_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/405_5_trasera.webp'
+WHERE id_cromo = 405;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/406_6_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/406_6_trasera.webp'
+WHERE id_cromo = 406;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/407_7_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/407_7_trasera.webp'
+WHERE id_cromo = 407;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/408_8_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/408_8_trasera.webp'
+WHERE id_cromo = 408;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/409_9_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/409_9_trasera.webp'
+WHERE id_cromo = 409;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/410_10_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/410_10_trasera.webp'
+WHERE id_cromo = 410;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/411_11_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/411_11_trasera.webp'
+WHERE id_cromo = 411;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/412_12_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/412_12_trasera.webp'
+WHERE id_cromo = 412;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/413_13_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/413_13_trasera.webp'
+WHERE id_cromo = 413;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/414_14_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/414_14_trasera.webp'
+WHERE id_cromo = 414;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/415_15_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/415_15_trasera.webp'
+WHERE id_cromo = 415;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/416_16_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/416_16_trasera.webp'
+WHERE id_cromo = 416;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/417_17_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/417_17_trasera.webp'
+WHERE id_cromo = 417;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/418_18_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/418_18_trasera.webp'
+WHERE id_cromo = 418;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/419_19_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/419_19_trasera.webp'
+WHERE id_cromo = 419;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/420_20_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/420_20_trasera.webp'
+WHERE id_cromo = 420;
+
+UPDATE cromos
+SET url_img_delantera = 'img/cromos/5_panini-fifa-world-cup-usa-94/457_257_delantera.webp',
+    url_img_trasera = 'img/cromos/5_panini-fifa-world-cup-usa-94/457_257_trasera.webp'
+WHERE id_cromo = 457;
